0263e3034963c0cffdc80b7343ee26115b883388
Cristina-Jordi <you@example.com>
2023-02-16 09:37:34 +0100

    Added functional Slideshow

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'cristinajordi.ch';
+export class AppComponent implements OnInit {
+  // title = 'cristinajordi.ch';
+  images = ['berge.jpg', 'iglu.jpg', 'wald.jpg'];
+  currentImage = 0;
+  showImage = true;
 
+  ngOnInit() {
+    this.updateImage();
+  }
+
+  updateImage() {
+    setInterval(()=>{
+      this.currentImage++;
+      this.currentImage = this.currentImage % this.images.length;  // % Restoperator: 3 Bilder : 3 = 0 ist der erste Index vom Bild also zählt er immer wieder von vorne 0,1,2...0,1,2 usw.
+      this.showImage = false;
+
+      setTimeout(()=>{
+        this.showImage = true;
+      }, 10);
+    }, 8000);
+  }
 }
